ff930e7c14a32df78c7d6c40ced75aa09812d856
JongKI-PARK <jkipark1@naver.com>
2023-06-10 22:47:54 +0900

    planner_hepler

--- enrollment_planner_helper/planner_helper.go
+++ enrollment_planner_helper/planner_helper.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"database/sql"
-	"encoding/json"
 	"log"
 	"net/http"
 	"strconv"
@@ -23,6 +22,13 @@ type Request struct {
 	SubjectID int `json:"subject_id"`
 }
 
+type Response struct {
+	Success   string `json:"success"`
+	Error     string `json:"error"`
+	StudentID int    `json:"studentId"`
+	SubjectID int    `json:"subjectId"`
+}
+
 func main() {
 	// MySQL 데이터베이스 연결 설정
 	db, err := sql.Open("mysql", "root:sys123457!@tcp(localhost:3306)/enrollment_planner")
@@ -40,18 +46,29 @@ func main() {
 		var request Request
 		err := c.BindJSON(&request)
 		if err != nil {
-			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			c.JSON(http.StatusBadRequest, Response{Success: "false", Error: err.Error()})
+			return
+		}
+
+		// 이미 수강신청이 존재하는지 확인
+		var existingPlan EnrollmentPlan
+		err = db.QueryRow("SELECT id, student_id, subject_id FROM enrollment_plan WHERE student_id = ? AND subject_id = ?", request.StudentID, request.SubjectID).Scan(&existingPlan.ID, &existingPlan.StudentID, &existingPlan.SubjectID)
+		if err == nil {
+			c.JSON(http.StatusBadRequest, Response{Success: "false", Error: "Enrollment plan already exists", StudentID: request.StudentID, SubjectID: request.SubjectID})
+			return
+		} else if err != sql.ErrNoRows {
+			c.JSON(http.StatusInternalServerError, Response{Success: "false", Error: err.Error()})
 			return
 		}
 
 		// enrollment_plan 테이블에 수강신청 정보 저장
 		_, err = db.Exec("INSERT INTO enrollment_plan (student_id, subject_id) VALUES (?, ?)", request.StudentID, request.SubjectID)
 		if err != nil {
-			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			c.JSON(http.StatusInternalServerError, Response{Success: "false", Error: err.Error(), StudentID: request.StudentID, SubjectID: request.SubjectID})
 			return
 		}
 
-		c.JSON(http.StatusOK, gin.H{"success": true})
+		c.JSON(http.StatusOK, Response{Success: "true", Error: "", StudentID: request.StudentID, SubjectID: request.SubjectID})
 	})
 
 	// 수강신청 계획 내역 조회 API 핸들러
@@ -59,14 +76,14 @@ func main() {
 		studentIDStr := c.Query("studentId")
 		studentID, err := strconv.Atoi(studentIDStr)
 		if err != nil {
-			c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid studentId"})
+			c.JSON(http.StatusBadRequest, Response{Success: "false", Error: "Invalid studentId"})
 			return
 		}
 
 		// enrollment_plan 테이블에서 studentID에 해당하는 과목 ID 조회
 		rows, err := db.Query("SELECT id, student_id, subject_id FROM enrollment_plan WHERE student_id = ?", studentID)
 		if err != nil {
-			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			c.JSON(http.StatusInternalServerError, Response{Success: "false", Error: err.Error()})
 			return
 		}
 		defer rows.Close()
@@ -79,21 +96,15 @@ func main() {
 			var plan EnrollmentPlan
 			err := rows.Scan(&plan.ID, &plan.StudentID, &plan.SubjectID)
 			if err != nil {
-				c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+				c.JSON(http.StatusInternalServerError, Response{Success: "false", Error: err.Error()})
 				return
 			}
 			enrollmentPlans = append(enrollmentPlans, plan)
 		}
 
 		// 조회 결과를 JSON 형식으로 반환
-		response, err := json.Marshal(enrollmentPlans)
-		if err != nil {
-			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
-			return
-		}
-
-		c.Header("Content-Type", "application/json")
-		c.String(http.StatusOK, string(response))
+		response := Response{Success: "true", Error: "", StudentID: studentID}
+		c.JSON(http.StatusOK, response)
 	})
 
 	// 서버 시작
